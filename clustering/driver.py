d373e7bfbb412fccd1dbc75b0f3ba71e3a91e25b
SaadBenn <Saadbennmushtaq@gmail.com>
2019-11-02 16:50:38 -0500

    Add the experiment driver where all functions reside in clustering directory

---
+++ clustering/driver.py
@@ -0,0 +1,42 @@
+
+def load_data():
+    """
+    This function is going to load the data - eventually it is going to hit an api endpoint
+    and get the data from there but for the time being we will be using a hardcoded file
+    :return:
+    """
+    return ""
+
+
+def cluster():
+    """
+    This function is going to apply k means clustering on the data set
+    :return:
+    """
+    return ""
+
+
+def assign_cluster_to_data():
+    """
+    This function will loop through the data provided from the cluster function and assign it to
+    different clusters
+    :return:
+    """
+    return ""
+
+
+def find_category_of_cluster():
+    """
+    This function returns the category of the cluster that is being passed to it
+    :return:
+    """
+    return ""
+
+
+def re_cluster():
+    """
+    This function is going to apply a second round of clustering if there is a mix of addresses in a cluster from the
+    first round of clustering
+    :return:
+    """
+    return ""
