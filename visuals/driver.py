82963198ea2dc72fb29f791bd42dfa9b53d28a55
Roya Lotfi <Roya.Lotfi93@gmail.com>
2019-11-06 17:40:37 -0600

    Visualization functions was added

---
+++ visuals/driver.py
@@ -0,0 +1,33 @@
+
+def show_transaction_graph(nodes):
+    '''
+    This function visualizes the transaction graph of a set of nodes
+    :param nodes:
+    :return:
+    '''
+
+def show_address_graph(nodes):
+    '''
+    This function viusualizes the address graph of a set of nodes
+    :param nodes:
+    :return:
+    '''
+
+def show_input_output_graph(nodes):
+    '''
+    This function shows the input-output graph of a set of nodes
+    :param nodes:
+    :return:
+    '''
+
+def visualize_network():
+    '''
+    This function shows the whole network of nodes
+    :return:
+    '''
+
+def visualize_sub_network(nodes):
+    '''
+    This function shows a subset of the whole blockchain network
+    :return:
+    '''
