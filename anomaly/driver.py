0c87c07e9f2d5ddb0009113b8e614b37905941b3
iamtanbirahmed <tanbir.sagar@gmail.com>
2019-11-06 16:57:16 -0600

    [fifth_labor] fifth_labor method signatures

--- anomaly/driver.py
+++ anomaly/driver.py
@@ -1,5 +1,3 @@
-
-
 # pump and dump prediction
 # get the data price for pump and dump prediction
 def load_price_data():
@@ -21,4 +19,44 @@ def predict_coin_possibility(given_coin):
 # pump and dump prediction
 # return all the coins with predicted possibilities greater than a chosen threshold (0.0 <= threshold <= 1.0)
 def get_predicted_pumped_coins(threshold: float):
+    return []
+
+
+# fifth labor
+# get list of transaction within a time period
+def get_transaction_list(start_time, end_time):
+    return []
+
+
+# fifth labor
+# get list of transactions related to the exchanges within a time period
+def get_exchange_transaction(start_time, end_time):
+    return []
+
+
+# fifth labor
+# build a local graph with the transaction_list, and markdown the transaction related to the exhchanges
+def build_graph(transaction_list, excange_transaction_list):
+    return "Graph edges and nodes"
+
+
+# fifth labor
+# detect the loop linking behaviour from the local graph and return the edges and nodes creating the loop
+def find_loop(edge_list, nodes):
+    return "Subgraph with detected loop"
+
+# fifth labor
+# predict the suspicious addresses from the loop subgraph
+def suspicious_addresses_from_loop(loop_subgraph):
+    return []
+
+
+# fifth labor
+# detect the loop spliting behaviour from the local graph and return the edges and nodes creating the split behaviour
+def find_split_nodes(edge_list, nodes):
+    return "Subgraph with split behaviour"
+
+# fifth labor
+# predict the suspicious addresses from the split subgraph
+def suspicious_addresses_from_split_nodes(split_subgraph):
     return []
