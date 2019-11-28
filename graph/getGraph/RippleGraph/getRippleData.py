950c7c4b0f0186cd55459189fabacb8a632be3f8
tadepask <tadepask@myumanitoba.ca>
2019-11-28 17:45:39 -0600

    Graph Analysis in Daily Basis:Chainlets(Occurrence and Amount),Occurrence and Amount of Chainlets in Matrix of 20X20,Level of Activity,Amount received to address in BTC,Number of Strongly Connected Components,Number of Weakly Connected Components in graph

--- graph/getGraph/RippleGraph/getRippleData.py
+++
@@ -1,8 +0,0 @@
-from anomaly.RippleAPI.data_api.ripple_data_api import RippleDataAPIClient
-import pprint
-
-api = RippleDataAPIClient('https://data.ripple.com')
-params = dict(limit="100", start='2019-01-01', type="Payment")
-transactions = api.get_transactions(**params)
-
-pprint.pprint(transactions)
