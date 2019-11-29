54b9cca3d0c18334a86ab9195566a93d9a48a50e
gurnoorkainth <kainthg1@myumanitoba.ca>
2019-11-28 20:05:39 -0600

    Added two programs: one pulls data from various exchanges and the second analyses the data obtained from first program to detect pump & dump schemes. Also adding a .zip file of the data obtained.

--- anomaly/driver.py
+++ anomaly/driver.py
@@ -22,34 +22,3 @@ def predict_coin_possibility(given_coin):
 # return all the coins with predicted possibilities greater than a chosen threshold (0.0 <= threshold <= 1.0)
 def get_predicted_pumped_coins(threshold: float):
     return []
-
-
-# ---- Pump and Dump - Kainth ----
-# Pulls the raw data from exchanges. Returns list of fetched symbols.
-# -- EXAMPLE INPUT --
-# exchange = 'binance'
-# from_date = '2019-04-20 00:00:00'
-# number_candles = 240 - number of candles
-# candle_size = '12h' - candlesticks
-# f_path = '../data' - CSV OHLCV file path for output
-def pull_data(exchange, from_date, number_candles, candle_size, f_path):
-    return 1
-
-
-# ---- Pump and Dump - Kainth ----
-# Gets symbol name from the csv file.
-def get_symbol(f_path):
-    return "XRP-BTC"  # Example
-
-
-# ---- Pump and Dump - Kainth ----
-# Main analysis method for pump and dump detection. Returns final dataframe with number of pump and dumps.
-# -- EXAMPLE INPUT --
-# symbol_name : "XRP-BTC" - symbol name provided by the exchange.
-# f_path : '../data' - output file from previously generated.
-# volume_thresh : 5 (500%) - volume threshold
-# price_thresh : 1.05 (5%) -  price threshold
-# window_size : 24 - size of the window for the rolling average (in hours)
-# candle_size = '12h' - candlesticks
-def analyse_symbol(symbol_name, f_path, volume_thresh, price_thresh, window_size, candle_size):
-    return 1
