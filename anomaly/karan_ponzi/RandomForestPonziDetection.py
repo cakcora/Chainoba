49c09e1ff4d37a28788fadba2edfc8eac4f026d0
kgovil2009 <56232912+kgovil2009@users.noreply.github.com>
2019-12-02 11:46:42 -0600

    Added NonPonzi addresses, made changes to data model, added visualization

--- anomaly/karan_ponzi/RandomForestPonziDetection.py
+++ anomaly/karan_ponzi/RandomForestPonziDetection.py
@@ -74,8 +74,8 @@ for addr_ponzi in ponzi:
 
     print(addr_ponzi, round(Bal * 10 ** -18, 4), round(N_maxpay * 10 ** -18, 4), N_investment, N_payment,
           round(Paid_rate, 2), ponzi_flag)
-    if (Bal > 0.0 and N_payment > 0.0 and N_investment > 0.0):
-        features = features.append({'Address': addr_ponzi, 'Bal': Bal * 10 ** -18, 'N_maxpay': N_maxpay * 10 ** -18,
+    #    if (Bal > 0.0 and N_payment > 0.0 and N_investment > 0.0):
+    features = features.append({'Address': addr_ponzi, 'Bal': Bal * 10 ** -18, 'N_maxpay': N_maxpay * 10 ** -18,
                                     'N_investment': N_investment, 'N_payment': N_payment, 'Paid_rate': Paid_rate,
                                     'Ponzi': ponzi_flag}, ignore_index=True)
 
@@ -113,15 +113,14 @@ for addr_non_ponzi in non_ponzi:
     Paid_rate = Paid_rate_division(N_payment, N_investment)
     print(addr_non_ponzi, round(Bal * 10 ** -18, 4), round(N_maxpay * 10 ** -18, 4), N_investment, N_payment,
           round(Paid_rate, 2), ponzi_flag)
-    if (Bal > 0.0 and N_payment > 0.0 and N_investment > 0.0):
-        features = features.append({'Address': addr_non_ponzi, 'Bal': Bal * 10 ** -18, 'N_maxpay': N_maxpay * 10 ** -18,
+    #    if (Bal > 0.0 and N_payment > 0.0 and N_investment > 0.0):
+    features = features.append({'Address': addr_non_ponzi, 'Bal': Bal * 10 ** -18, 'N_maxpay': N_maxpay * 10 ** -18,
                                     'N_investment': N_investment, 'N_payment': N_payment, 'Paid_rate': Paid_rate,
                                     'Ponzi': ponzi_flag}, ignore_index=True)
 
 '''
 w,h = 150, 7
 Matrix = [[0 for x in range(w)] for y in range(h)]
-
 numpy [0][0] = 'Address'
 Matrix [0][1] = 'Bal'
 Matrix [0][2] = 'N_maxpay'
@@ -129,9 +128,7 @@ Matrix [0][3] = 'N_investment'
 Matrix [0][4] = 'N_payment'
 Matrix [0][5] = 'Paid_rate'
 Matrix [0][6] = 'Ponzi'
-
 print(Matrix)
-
 '''
 
 print(features)
@@ -150,32 +147,168 @@ features = np.array(features)
 from sklearn.model_selection import train_test_split
 
 # Split the data into training and testing sets
-train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.25,
-                                                                            random_state=42)
+train, test, train_labels, test_labels = train_test_split(features, labels, test_size=0.25,
+                                                          random_state=42)
 
-print('Training Features Shape:', train_features.shape)
+print('Training Features Shape:', train.shape)
 print('Training Labels Shape:', train_labels.shape)
-print('Testing Features Shape:', test_features.shape)
+print('Testing Features Shape:', test.shape)
 print('Testing Labels Shape:', test_labels.shape)
 print(test_labels)
 
+train = train.astype('bool')
+train_labels = train_labels.astype('bool')
+test = test.astype('bool')
+test_labels = test_labels.astype('bool')
+
 # Import the model we are using
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
 
 # Instantiate model with 1000 decision trees
-rf = RandomForestRegressor(n_estimators=1000, random_state=42)
+rf = RandomForestClassifier(n_estimators=1000, random_state=42)
 # Train the model on training data
-rf.fit(train_features, train_labels)
+rf.fit(train, train_labels)
 
 # Use the forest's predict method on the test data
-predictions = rf.predict(test_features)
+predictions = rf.predict(test)
+print(predictions)
 # Calculate the absolute errors
-errors = abs(predictions - test_labels)
+errors = abs(predictions ^ test_labels)
+print(errors)
+
+'''
 # Print out the mean absolute error (mae)
 print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
 
 # Calculate mean absolute percentage error (MAPE)
-mape = 100 * Paid_rate_division(errors, test_labels)
+mape = 100 * Paid_rate_division(errors, test_labels.all())
 # Calculate and display accuracy
 accuracy = 100 - np.mean(mape)
 print('Accuracy:', round(accuracy, 2), '%.')
+'''
+n_nodes = []
+
+max_depths = []
+
+# Stats about the trees in random forest
+for ind_tree in rf.estimators_:
+    n_nodes.append(ind_tree.tree_.node_count)
+    max_depths.append(ind_tree.tree_.max_depth)
+
+print(f'Average number of nodes {int(np.mean(n_nodes))}')
+print(f'Average maximum depth {int(np.mean(max_depths))}')
+
+# Training predictions (to demonstrate overfitting)
+train_rf_predictions = rf.predict(train)
+train_rf_probs = rf.predict_proba(train)[:, 1]
+
+# Testing predictions (to determine performance)
+rf_predictions = rf.predict(test)
+rf_probs = rf.predict_proba(test)[:, 1]
+
+from sklearn.metrics import precision_score, recall_score, roc_auc_score, roc_curve
+import matplotlib.pyplot as plt
+
+# Plot formatting
+plt.style.use('fivethirtyeight')
+plt.rcParams['font.size'] = 18
+
+
+def evaluate_model(predictions, probs, train_predictions, train_probs):
+    """Compare machine learning model to baseline performance.
+    Computes statistics and shows ROC curve."""
+
+    baseline = {}
+
+    baseline['recall'] = recall_score(test_labels,
+                                      [1 for _ in range(len(test_labels))])
+    baseline['precision'] = precision_score(test_labels,
+                                            [1 for _ in range(len(test_labels))])
+    baseline['roc'] = 0.5
+
+    results = {}
+
+    results['recall'] = recall_score(test_labels, predictions)
+    results['precision'] = precision_score(test_labels, predictions)
+    results['roc'] = roc_auc_score(test_labels, probs)
+
+    train_results = {}
+    train_results['recall'] = recall_score(train_labels, train_predictions)
+    train_results['precision'] = precision_score(train_labels, train_predictions)
+    train_results['roc'] = roc_auc_score(train_labels, train_probs)
+
+    for metric in ['recall', 'precision', 'roc']:
+        print(
+            f'{metric.capitalize()} Baseline: {round(baseline[metric], 2)} Test: {round(results[metric], 2)} Train: {round(train_results[metric], 2)}')
+
+    # Calculate false positive rates and true positive rates
+    base_fpr, base_tpr, _ = roc_curve(test_labels, [1 for _ in range(len(test_labels))])
+    model_fpr, model_tpr, _ = roc_curve(test_labels, probs)
+
+    plt.figure(figsize=(8, 6))
+    plt.rcParams['font.size'] = 16
+
+    # Plot both curves
+    plt.plot(base_fpr, base_tpr, 'b', label='baseline')
+    plt.plot(model_fpr, model_tpr, 'r', label='model')
+    plt.legend();
+    plt.xlabel('False Positive Rate');
+    plt.ylabel('True Positive Rate');
+    plt.title('ROC Curves');
+    plt.show();
+
+
+evaluate_model(rf_predictions, rf_probs, train_rf_predictions, train_rf_probs)
+plt.savefig('roc_auc_curve.png')
+
+from sklearn.metrics import confusion_matrix
+import itertools
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Oranges):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    Source: http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    # Plot the confusion matrix
+    plt.figure(figsize=(10, 10))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title, size=24)
+    plt.colorbar(aspect=4)
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45, size=14)
+    plt.yticks(tick_marks, classes, size=14)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+
+    # Labeling the plot
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), fontsize=20,
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.grid(None)
+    plt.tight_layout()
+    plt.ylabel('True label', size=18)
+    plt.xlabel('Predicted label', size=18)
+
+
+# Confusion matrix
+cm = confusion_matrix(test_labels, rf_predictions)
+plot_confusion_matrix(cm, classes=['Poor Health', 'Good Health'],
+                      title='Health Confusion Matrix')
+
+plt.savefig('cm.png')
