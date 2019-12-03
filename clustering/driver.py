e8a875140bf32665cf5d29468cbb8e97a26af818
SaadBenn <saadbennmushtaq@gmail.com>
2019-12-03 16:47:32 -0600

    Add silhouette K means clustering implementation
    Signed-off-by: SaadBenn <Saadbennmushtaq@gmail.com>

--- clustering/driver.py
+++ clustering/driver.py
@@ -1,4 +1,3 @@
-import numpy as np
 
 """
 This file will have all the functions related to clustering
@@ -6,16 +5,38 @@ This file will have all the functions related to clustering
 - clustering-classes
   - driver.py
 """
+import clustering.src.silhouette_k_cluster
 
 
 def load_data():
     """
-    This function is going to load the data - eventually it is going to hit an api endpoint
-    and get the data from there but for the time being we will be using a hardcoded file
-    :return:
+    This function is going to load the data into a dataframe by calling the functions provided
+    by the graph time
+    :return: dataframe - please see src/silhouette_k_clustering for more detail
     """
-    return ""
+    return clustering.src.silhouette_k_cluster.load_data()
 
+
+def cluster():
+    """
+    Applies K means clustering algorithm and builds a scatter plot for visualization
+    """
+    df = clustering.src.silhouette_k_cluster.load_data()
+    num_cluster = clustering.src.silhouette_k_cluster.find_optimal_num_cluster(df)
+    data_frame = clustering.src.silhouette_k_cluster.data_pipeline(df)
+    cl = clustering.src.silhouette_k_cluster.cluster(data_frame, num_cluster)
+    tsne_plotter(cl, data_frame)
+
+
+def tsne_plotter(cl, data_frame):
+    """
+    Plots a 3D t-SNE for the data
+    :param cl: cluster fit data
+    :param data_frame: the actual dataframe
+    :return: plots a t-sne graph
+    """
+    tsne_results = clustering.src.silhouette_k_cluster.calc_tsne(data_frame)
+    clustering.src.silhouette_k_cluster.plot_tsne(cl, tsne_results)
 
 
 def handle_missing_values():
@@ -45,6 +66,7 @@ def TransitiveClosure():
     Transitive closure is for over all the transactions. For example, if there is one transaction in which 1 and 2 are
     used as sending addresses, and another transaction in which 2 and 3 are used as sending addresses, we conclude that
     all three addresses are jointly owned.
+    """
 
 
 
@@ -66,38 +88,6 @@ def data_preprocessing():
 
 
 
-def cluster():
-    """
-    This function is going to apply k means clustering on the data set
-    :return:
-    """
-    return ""
-
-
-def assign_cluster_to_data():
-    """
-    This function will loop through the data provided from the cluster function and assign it to
-    different clusters
-    :return:
-    """
-    return ""
-
-
-def find_category_of_cluster():
-    """
-    This function returns the category of the cluster that is being passed to it
-    :return:
-    """
-    return ""
-
-
-def re_cluster():
-    """
-    This function is going to apply a second round of clustering if there is a mix of addresses in a cluster from the
-    first round of clustering
-    :return:
-    """
-    return ""
 
 
 def ward_method_clustering(G):
--- clustering/src/silhouette_k_cluster.py
+++ clustering/src/silhouette_k_cluster.py
@@ -1,5 +1,3 @@
-import nltk
-from nltk.cluster.kmeans import KMeansClusterer
 from sklearn.preprocessing import FunctionTransformer, StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
@@ -10,12 +8,13 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from graph import *
-from functools import reduce
-
 import graph.getGraph.addressFeatures
 
+# terminal can only output certain number of columns
 pd.set_option('display.max_columns', 10)
+
+# TODO: Graph team has yet to turn this into one function call
+# The workaround for the team being is to merge the different dataframes
 res, df0 = graph.getGraph.addressFeatures.getFeatures(9, 1, 2009, 'LEVEL_OF_ACTIVITY')
 res, df = graph.getGraph.addressFeatures.getFeatures(9, 1, 2009, 'TOTAL_BTC_RECEIVED')
 res, df1 = graph.getGraph.addressFeatures.getFeatures(9, 1, 2009, 'TOTAL_BTC_SENT')
@@ -23,44 +22,27 @@ res, df2 = graph.getGraph.addressFeatures.getFeatures(9, 1, 2009, 'CURRENT_BALAN
 
 
 def load_data():
+    """
+    Call the graph's team function to get the dataframe
+    Merge the different dataframes
+    :return: dataframe with the first column dropped
+    """
     data_frame = pd.concat([df0, df, df1, df2], ignore_index=True, sort=True).fillna(0)
     cols = ['Node', 'Bitcoin Sent', 'Amount', 'Current Balance', 'total_degree']
     data_frame = data_frame[cols]
 
-    print(data_frame)
+    # print(data_frame)
     cleaned_df = data_frame.drop('Node', 1)
-    print(cleaned_df)
     # print(cleaned_df)
-    # standardizeData(data_frame)
-    # num_cluster = find_optimal_num_cluster(cleaned_df)
-    # cluster(cleaned_df, num_cluster)
-    # calc_tsne(data_frame)
     return cleaned_df
 
 
-# def standardizeData(df):
-#     features = ['Bitcoin Sent', 'Amount', 'Current Balance', 'total_degree']
-#
-#     # Separating out the features
-#     x = df.loc[:, features].values
-#
-#     # Separating out the target
-#     y = df.loc[:, ['Node']].values
-#
-#     # Standardizing the features
-#     x = StandardScaler().fit_transform(x)
-#
-#     pca = PCA(n_components=2)
-#     principalComponents = pca.fit_transform(x)
-#     print(principalComponents)
-    # principalDf = pd.DataFrame(data=principalComponents, columns=['principal component 1', 'principal component 2'])
-    # print(principalDf)
-    # print(x)
-    # finalDf = pd.concat([principalDf, df[['amount']]], axis=1)
-    # print(finalDf)
-
-
 def cluster(cleaned_df, num_cluster=8):
+    """
+    :param cleaned_df: the dataframe
+    :param num_cluster: optimal number of clusters
+    :return: a cluster fit of the data
+    """
     kmeans = KMeans(num_cluster)
     cl = kmeans.fit(cleaned_df)
     centroids = kmeans.predict(cleaned_df)
@@ -72,6 +54,11 @@ def cluster(cleaned_df, num_cluster=8):
 
 
 def data_pipeline(dataframe):
+    """
+    Builds a pipeline and standardizes data
+    :param dataframe:
+    :return: pipe and the standardized data
+    """
     log = FunctionTransformer(func=np.log1p, inverse_func=np.expm1, validate=True)
     scale = StandardScaler()
     pca = PCA(n_components=dataframe.shape[1])
@@ -81,11 +68,15 @@ def data_pipeline(dataframe):
                      ('scale', scale),
                      ('PCA', pca)])
 
-    results = pipe.fit_transform(data_frame)
+    results = pipe.fit_transform(dataframe)
     return pipe, results
 
 
 def find_optimal_num_cluster(cleaned_df):
+    """
+    :param cleaned_df:
+    :return: optimal cluster number
+    """
     sil = []
     kmax = list(range(2, 10))
     for k in kmax:
@@ -100,62 +91,47 @@ def find_optimal_num_cluster(cleaned_df):
     plt.show()
     return max(sil)
 
-#
-# def recluster(df, clust, clusters, n_clusters):
-#     subcluster = cluster(results, n_clusters)
-#     kcluster = KMeansClusterer(n_clusters, distance=nltk.cluster.util.cosine_distance, repeats=50)
-#     assigned_clusters = kcluster.cluster(results, assign_clusters=True)
-#     # assign new cluster labels
-#     subcluster.labels_ = np.array(assigned_clusters)
-#     subcluster.cluster_centers_ = np.array(kcluster.means())
-#
-#     return subcluster, results
-#
-#
-
 
 def calc_tsne(results, n_components=2, perplexity=20, n_iter=300, verbose=1):
-    pipe, df = data_pipeline(results)
+    """
+    :param results: the dataframe
+    :return: t-sne result
+    """
+    data_pipeline(results)
     time_start = time.time()
-    tsne  = TSNE(n_components=n_components, perplexity=perplexity, n_iter=n_iter, verbose=verbose, learning_rate=100)
+    tsne = TSNE(n_components=n_components, perplexity=perplexity, n_iter=n_iter, verbose=verbose, learning_rate=100)
     tsne_results = tsne.fit_transform(results)
-    print('TSNE done! Time elapsed: {} seconds'.format(time.time()-time_start))
+    print('T-SNE done! Time elapsed: {} seconds'.format(time.time() - time_start))
     return tsne_results
 
+
 def plot_tsne(cl, tsne_results):
+    """
+    :param cl: the clustered fit
+    :param tsne_results
+    plots t-sne
+    """
     NUM_COLORS = cl.n_clusters
     cm = plt.get_cmap('nipy_spectral')
 
-    fig = plt.figure(figsize=(15,12))
+    fig = plt.figure(figsize=(15, 12))
     ax = fig.add_subplot(111)
-    ax.set_color_cycle([cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)])
+    ax.set_color_cycle([cm(1. * i / NUM_COLORS) for i in range(NUM_COLORS)])
 
     for c in np.unique(cl.labels_):
         mask = cl.labels_ == c
         if np.sum(mask) == c:
-            lbl = '_noleged_'
+            lbl = '_nolegend_'
         else:
             lbl = c
 
         plt.scatter(tsne_results[mask][:, 0], tsne_results[mask][:, 1], s=20, alpha=.4, label=lbl)
 
-    legend = plt.legend(bbox_to_anchor=(1,1))
+    legend = plt.legend(bbox_to_anchor=(1, 1))
     for lh in legend.legendHandles:
         lh.set_alpha(1)
 
-
-    plt.title('TSNE', fontsize=20)
+    plt.title('T-SNE', fontsize=20)
     plt.xlabel('first principal component')
     plt.ylabel('second principal component')
     plt.show()
-#
-# df = load_data()
-# pipe, results = data_pipeline(df)
-# print(results)
-# cluster(results, 8)
-
-data_frame = load_data()
-num_cluster = find_optimal_num_cluster(data_frame)
-data_frame = data_pipeline(data_frame)
-cluster(data_frame, num_cluster)
-tsne_results = calc_tsne(data_frame)
