f2da3c0450db6aa69d973629e67239c201ab903c
baha <baharababah@yahoo.com>
2019-12-06 14:19:35 -0600

    headers, comments, and example driver file.

--- clustering/CommonSpending.py
+++ clustering/CommonSpending.py
@@ -8,7 +8,15 @@ def CommonSpending(d, m, y, Offset):
     """
 This function  is for implementing CommonSpending Heuristic: If two or more addresses are inputs of the same transaction
 with one output, then all these addresses are controlled by the same user.
-the function will create csv file in each row it has the transaction hash and the input addresses of the transaction.
+The function will create csv file (in clustering folder, called CommonSpending)
+ in each row it has the transaction hash and the input addresses of the transaction.
+
+It takes the values that is required to generate COMPOSITE graph(day, month, year, and offset), to pass it to
+TransactionsWithoutDuplications function which will also pass it to loadData function.
+
+Reference:
+Ermilov D, Panov M, Yanovich Y. Automatic Bitcoin address clustering.
+In2017 16th IEEE International Conference on Machine Learning and Applications (ICMLA) 2017 Dec 18 (pp. 461-466). IEEE.
 
     """
 
@@ -19,25 +27,27 @@ the function will create csv file in each row it has the transaction hash and th
         writer = csv.writer(csvfile)
         writer.writerow(['Transaction'] + ['First Input Address'] + ['Second Input Address'] + ['Third Input Address'])
 
-
+    # for loop to check all the transactions in the COMPOSITE graph.
     for x in range(NumberOfTxsTo):
         num_input_addresses = 0.
         num_out_addresses = 0
         InputAddresses = []
         OutAddresses = []
 
-        for row in arr:
 
+        # find number of input addresses, for the transaction.
+        for row in arr:
             if row[1] == TransactionsTo[x] and row[0] not in InputAddresses:
                 num_input_addresses = num_input_addresses + 1
                 InputAddresses.append(row[0])
+        # find number of output addresses for the transaction.
 
         for row in arr:
-
             if row[0] == TransactionsTo[x] and row[1] not in OutAddresses:
                 num_out_addresses = num_out_addresses + 1
                 OutAddresses.append(row[1])
 
+         # check if number of input addresses greater than one, and one output address then they are belong to same user.
         if len(InputAddresses) >= 2 and len(OutAddresses) == 1:
             with open('CommonSpending.csv', 'a', newline='') as csvfile:
                 writer = csv.writer(csvfile)
