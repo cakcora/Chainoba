db6d595bbe498130415624961356f800d9d7e016
Dilawer <dilaw3r@gmail.com>
2019-12-07 23:35:11 -0600

    Update k-core to class implemetation

--- influence/kcore.py
+++ influence/kcore.py
@@ -1,49 +1,34 @@
 import networkx as nx
 from visuals.show_graph.show_address_graph import address_graph
 
-
 class KCore:
 
     def __init__(self, graph):
         self.graph = graph
 
-    # Get k-core sub-graph for a given graph 'G' and of specified coreness 'k'
+    # Return the k-core of this graph
+    #   k - [int, optional] The order of the core. If not specified return the main core.
+    #   core_number : [dictionary, optional] Precomputed core numbers for the graph G.
     def get_k_cores(self, k, core):
         return nx.k_core(self.graph, k)
 
+    # This function will return a dictionary keyed by node to the core number.
     def get_core_number(self):
         return nx.core_number(self.graph)
 
-    # Maximum k-core sub-graph for a given graph 'G'
+    # Maximum k-core sub-graph for this graph
     def max_k_core_graph(self):
         return nx.k_core(self.graph)
 
+    # Get the graph associated with the object instance
     def get_graph(self):
         return self.graph
 
-    def make_graph(self):
-
-        graph2 = address_graph()
-
-        # the fist list, represents input addresses the second is output addresses and the last list represents the corresponding bitcoin amount
-        # transferred. address a1 has sent 1 bitcoin to a1. a2 has sent 1 bitcoin to a1
-
-        graph2.add_address_node(["a1", "a2", "a3", "a4"], ["a1", "a1", "a5", "a3"], [1, 1, 1, 2])
-        graph2.add_address_node(["a4", "a5", "a3", "a6"], ["a4", "a1", "a5", "a3"], [1, 3, 1, 2])
-        graph2.add_address_node(["a1"], ["a3"], [1, 1, 1, 2])
-        graph2.add_address_node(["a9"], ["a19"], [1, 1, 1, 2])
-        graph2.add_address_node(["a1", "a5", "a3", "a6"], ["a2", "a6", "a4", "a7"], [1, 3, 1, 2])
-
-        inputs = ["a7", "a8", "a9", "a10"]
-        output = ["a11", "a12", "a13", "a14"]
-        amount = [1, 3, 1, 2]
-
-        graph2.add_address_node(inputs, output, amount)
-
-        graph2.show_graph()
-
-        # g = address_graph()
-        # # Populate using loop?
-        # g.add_address_node(1, 2, 3)
-        # graph.show_graph()
-
+    # Generate view for a given graph
+    #     - Provide list of all input addresses
+    #     - Provide list of all output addresses
+    #     - Provide list of all amounts between input and output addresses
+    def generate_graph(self, inputs, outputs, amount):
+        graph = address_graph()
+        graph.add_address_node(inputs, outputs, amount)
+        graph.show_graph()
--- influence/src/kcoreeval.py
+++ influence/src/kcoreeval.py
@@ -1,26 +1,14 @@
-from influence.generator import generate_undirected_graph
 from influence.generator import generate_bitcoin_graph_from_edgelist
-from visuals.show_graph.show_address_graph import address_graph
-
 from influence.kcore import KCore
 
-from graph.getGraph.ethereumGraph.getAPIData import getEthereumgraph
+# from graph.getGraph.ethereumGraph.getAPIData import getEthereumgraph
 from graph.getGraph.getAPIData import getGraph
-
-import matplotlib.pyplot as plt
-import random
 import networkx as nx
 
-# G = getGraph(9, 2, 2009, 1, 'ADDRESS')[1]
+graph1 = KCore(generate_bitcoin_graph_from_edgelist())
 
-G = generate_bitcoin_graph_from_edgelist()
+inputs = ["a2", "a3"]
+outputs = ["a4", "a2"]
+amount = [5, 1]
 
-g1 = KCore(G)
-
-g1.make_graph()
-
-# H = g1.max_k_core_graph()
-# pos = nx.spring_layout(H)
-# nx.draw(H, pos, node_size=300, weight=.1)
-# plt.show()
-
+graph1.generate_graph(inputs, outputs, amount)
