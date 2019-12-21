1284dce6294c36c26e135c5617936bb427eca027
Gagan Bath <gagan.bath16@gmail.com>
2019-12-21 12:28:06 +0530

    Mint Centrality updated

---
+++ influence/src/mintcentrality.py
@@ -0,0 +1,9 @@
+
+
+def mint_centrality_for_graph(G):
+    """
+        Calculates and returns the Mint centrality for each node in the graph.
+
+        :param G: NetworkX.Graph
+        :return : Dictionary where key is the index of each node, and value is the degree centrality of the i_th node
+        """
